eb671bc4d74c6e84a3e14d6f739f2e7e1a78c5b2
Frances72 <francesanderson3@gmail.com>
2022-06-25 15:32:26 +0200

    Customer controller and CRDEIndex Views
    Signed-off-by: Frances72 <francesanderson3@gmail.com>

--- ViewModels/RandomMovieViewModel.cs
+++ ViewModels/RandomMovieViewModel.cs
@@ -5,6 +5,6 @@ namespace Vidly.ViewModels
     public class RandomMovieViewModel
     {
         public Movie Movie {get;set;}
-      //  public List<Customer> Customers {get;set;}
+        public List<Customer> Customers {get;set;}
     }
 }
